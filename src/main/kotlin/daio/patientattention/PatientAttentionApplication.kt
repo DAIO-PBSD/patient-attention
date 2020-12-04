1d1c4bd71e254687f6c63dc9e09eb04604babb24
Dorian <dorian.r.mv@gmail.com>
2020-12-04 09:46:46 -0500

    Project Created

---
+++ src/main/kotlin/daio/patientattention/PatientAttentionApplication.kt
@@ -0,0 +1,11 @@
+package daio.patientattention
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PatientAttentionApplication
+
+fun main(args: Array<String>) {
+	runApplication<PatientAttentionApplication>(*args)
+}
